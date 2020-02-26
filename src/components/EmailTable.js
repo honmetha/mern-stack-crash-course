40282470e545373454f9daae2cacd52d1b568afa
Hon <dennis.mejen@gmail.com>
2020-02-26 21:35:54 +0700

    Add part 1 & 2

--- src/components/EmailTable.js
+++ src/components/EmailTable.js
@@ -1,31 +1,88 @@
-import React, { Component } from 'react';
-import './components.css';
+import React from "react";
+import "./components.css";
 
-export default class EmailTable extends Component {
-  render() {
-    let { emails } = this.props;
-    return (
-      <div className="emailtable-centerdiv">
-        <table className="emailtable-table">
-          <thead>
+export default props => {
+  const { emailsList, ranksList, hasSent } = props;
+  const getRank = rankId => {
+    let reviewsLeftToUprank = "";
+    ranksList.forEach(rank => {
+      if (rank[0] === rankId) {
+        reviewsLeftToUprank = rank[1];
+      }
+    });
+    return reviewsLeftToUprank;
+  };
+  const getMessage = element => {
+    let message = "";
+    if (element[5]) {
+      message = "กำลังส่ง";
+      return message;
+    }
+    if (hasSent) {
+      if (element[5]) {
+        message = "กำลังส่ง";
+      } else {
+        if (element[4]) {
+          message = "ส่งสำเร็จ";
+        } else {
+          message = "ส่งล้มเหลว";
+        }
+      }
+    } else {
+      message = "รอส่ง";
+    }
+    return message;
+  };
+
+  const getColor = element => {
+    let color = "";
+    if (element[5]) {
+      color = "blue";
+      return color;
+    }
+    if (hasSent) {
+      if (element[5]) {
+        color = "blue";
+      } else {
+        if (element[4]) {
+          color = "green";
+        } else {
+          color = "red";
+        }
+      }
+    } else {
+      color = "black";
+    }
+    return color;
+  };
+
+  return (
+    <div className="emailtable-centerdiv">
+      <table className="emailtable-table">
+        <thead>
           <tr>
             <td>อีเมล</td>
             <td>ข้อความ</td>
             <td>สถานะ</td>
           </tr>
-          </thead>
-          <tbody>
-          {this.props.emailsList.map((element, index)=>(
+        </thead>
+        <tbody>
+          {emailsList.map((element, index) => (
             <tr key={index}>
               <td>{element[0]}</td>
-              <td>{element[1]}</td>
-              <td>READY</td>
+              <td>
+                สวัสดี คุณ {element[1]}
+                <br /> อีก {element[3]} รีวิว คุณจะได้เป็น{" "}
+                {`${getRank(element[2])} `}
+                ร่วมแบ่งปันรีวิวกับเพื่อนสมาชิกกันนะคะ
+              </td>
+              <td>
+                <label style={{color: getColor(element)}}>{getMessage(element)}</label>
+              </td>
             </tr>
-          ))
-          }
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-}
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
