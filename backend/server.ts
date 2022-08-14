9b7df27ef9e3dc363fdf67a55e53e261ec8eecdd
Hon <dennis.mejen@gmail.com>
2022-08-15 00:09:48 +0700

    📦 add: 2 - Express App Setup (Part 2)

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,24 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // express app
 const app: Express = express();
+const port = process.env.PORT;
+
+// middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.get("/", (req: Request, res: Response) => {
+  res.json({ mssg: "Welcome to the app" });
+});
 
 // listen for requests
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log("listening on port 4000");
 });
