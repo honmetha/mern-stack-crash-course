466a92fac7efac1fdca2c932272328eac70c8504
Hon <dennis.mejen@gmail.com>
2022-08-16 00:25:07 +0700

    📦 add: 4 - MongoDB Atlas & Mongoose

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import workoutRoutes from "./routes/workouts";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -19,7 +20,13 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 // routes
 app.use("/api/workouts", workoutRoutes);
 
-// listen for requests
-app.listen(port, () => {
-  console.log("listening on port 4000");
-});
+// connect to db
+mongoose
+  .connect(`${process.env.MONGO_URI}`)
+  .then(() => {
+    // listen for requests
+    app.listen(port, () => {
+      console.log("connected to db & listening on port", port);
+    });
+  })
+  .catch((error) => console.log(error));
