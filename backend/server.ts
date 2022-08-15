c0d7a144fb1393fe8d2cea024daa349595382e53
Hon <dennis.mejen@gmail.com>
2022-08-15 23:53:04 +0700

    📦 add: 3 - Express Router & API Routes

--- backend/server.ts
+++ backend/server.ts
@@ -5,18 +5,19 @@ dotenv.config();
 
 // express app
 const app: Express = express();
+const workoutRoutes = require("./routes/workouts");
 const port = process.env.PORT;
 
 // middleware
+app.use(express.json());
+
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req: Request, res: Response) => {
-  res.json({ mssg: "Welcome to the app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 // listen for requests
 app.listen(port, () => {
