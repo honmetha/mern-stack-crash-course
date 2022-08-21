fb4967695208b57f79f56a9c00147b062201df08
Hon <dennis.mejen@gmail.com>
2022-08-21 23:50:34 +0700

    📦 add: 9 - Fetching Data (Part 1)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,39 @@
+import * as React from "react";
+
+interface IWorkout {
+  _id: String;
+  title: String;
+  reps: Number;
+  load: Number;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: Number;
+}
+
 const Home = () => {
+  const [workouts, setWorkouts] = React.useState<any>(null);
+
+  React.useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("http://localhost:3000/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout: any) => (
+            <p key={workout._id}>{workout.title}</p>
+          ))}
+      </div>
     </div>
   );
 };
