ebb85a54526b8357fe305863a6908b5a88d54237
Hon <dennis.mejen@gmail.com>
2022-08-25 23:34:46 +0700

    📦 add: 11 - Adding React Context (Part 2)

--- frontend/src/context/WorkoutContext.tsx
+++ frontend/src/context/WorkoutContext.tsx
@@ -4,12 +4,35 @@ interface IWorkoutsContextProvider {
   children: React.ReactNode;
 }
 
-export const WorkoutsContext = React.createContext<null>(null);
+// fix typescript type
+export const WorkoutsContext = React.createContext<any>(null);
+
+// fix typescript type
+export const workoutsReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state],
+      };
+    default:
+      return state;
+  }
+};
 
 export const WorkoutsContextProvider = ({
   children,
 }: IWorkoutsContextProvider) => {
+  const [state, dispatch] = React.useReducer(workoutsReducer, {
+    workouts: null,
+  });
+
   return (
-    <WorkoutsContext.Provider value={null}>{children}</WorkoutsContext.Provider>
+    <WorkoutsContext.Provider value={{ state, dispatch }}>
+      {children}
+    </WorkoutsContext.Provider>
   );
 };
---
+++ frontend/src/hooks/useWorkoutsContext.ts
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { WorkoutsContext } from "../context/WorkoutContext";
+
+export const useWorkoutsContext = () => {
+  const context = React.useContext(WorkoutsContext);
+
+  if (!context) {
+    throw Error(
+      "useWorkoutsContext must be used inside a WorkoutsContextProvider"
+    );
+  }
+
+  return context;
+};
