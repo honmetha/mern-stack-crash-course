fde15b6298c548b6e76c8dea5d1203f59b851519
Hon <dennis.mejen@gmail.com>
2022-08-31 23:39:49 +0700

    📦 add: 3 - Signing Up & Hashing Passwords (Part 1)

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
 interface IUserSchema {
   email: {
@@ -20,5 +21,17 @@ const userSchema = new Schema<IUserSchema>({
   },
   password: { type: String, required: true },
 });
+
+// static signup method
+userSchema.statics.signup = async (email, password) => {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+};
 
 export default model("User", userSchema);
